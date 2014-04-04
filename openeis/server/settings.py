1b949f6703a9da769e1f3095d1de1e7ec5dba844
Brandon Carpenter <brandon.carpenter@pnnl.gov>
2014-04-04 15:18:48 -0700

    Updated the help blurb in settings.py

--- openeis/server/settings.py
+++ openeis/server/settings.py
@@ -1,17 +1,20 @@
-# Never commit this file to revision control; it contains sensitive
-# information. General settings should be written to _settings.py.
-# Sites may override general settings by including them below the
-# following import statements. Optionally, sites may implement the
-# openeis.local package and include site-specific settings in that
-# packages settings module, which are preferred over _settings. That
-# module may also import _settings before the site-specific settings.
+# WARNING: Never commit this file to revision control; it may contain
+# sensitive information.
+#
+# The preferred method of overriding configuration is to create an
+# openeis.local package and override openeis.server._settings in its
+# settings.py module. The openeis.local package must be in the
+# PYTHONPATH so that this module can find it and include its settings.
+#
+# If you ignore the warning above and place site-specific configuration
+# in this file and it is under git version control, be sure to mark it
+# as unchanged using git to help prevent accidentally committing changes:
+#
+#     git update-index --assume-unchanged settings.py
+#
 
 try:
     from openeis.local.settings import *
 except ImportError:
     from ._settings import *
-
-# Include site-specific settings below this line or in
-# openeis.local.settings (which must be on the PYTHONPATH).
-
 
