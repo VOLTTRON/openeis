7efde921ad248d626a741641a29843ba88aae43d
Craig Allwardt <craig.allwardt@pnnl.gov>
2014-04-09 18:55:59 -0700

    broke elements into key value and other pairs.

--- openeis/projects/scoop_units.py
+++ openeis/projects/scoop_units.py
@@ -21,7 +21,32 @@ with open("hastack_units.txt") as parser:
             units['units'][current_unit] = []
             continue
         
-        units['units'][current_unit].append(line.strip())
+        print(line.strip())
+        fields = line.strip().split(',')
+        
+        if len(fields) == 1:
+            key = value = fields[0]
+        else:
+            key = fields[0]
+            value = fields[1]
+                    
+        other = fields[2:]
+        if len(other) == 0:
+            other = None
+        else:
+            if len(other) == 1:
+                other = other[0]
+        
+        # Only include other keyword if there are other items in the list  
+        if other:
+            units['units'][current_unit].append({"key": key,
+                                                 "value": value,
+                                                 "other": other
+                                                 })
+        else:
+            units['units'][current_unit].append({"key": key,
+                                                 "value": value
+                                                 })
         
 
 with open("units.json", 'w') as writeJson:
