2704ccb14aacdcf40f903457533c5c0ea848d292
Tim Kang <tim.kang@pnnl.gov>
2014-04-21 17:42:26 -0700

    Display file list as table

--- openeis-ui/js/file.js
+++ openeis-ui/js/file.js
@@ -29,4 +29,14 @@ angular.module('openeis-ui.file', [])
             };
         },
     };
+})
+// From https://gist.github.com/thomseddon/3511330
+.filter('bytes', function() {
+    return function(bytes, precision) {
+        if (isNaN(parseFloat(bytes)) || !isFinite(bytes)) return '--';
+        if (typeof precision === 'undefined') precision = 0;
+        var units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB'],
+        number = Math.floor(Math.log(bytes) / Math.log(1024));
+        return (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) + ' ' + units[number];
+    };
 });
