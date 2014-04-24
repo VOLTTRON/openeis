792b67c192c738729233079741f1c3a3bbf1fd5e
Poorva Sharma <poorva.sharma@pnnl.gov>
2014-04-24 16:08:07 -0700

    Designed and implemented meta-data schema for describing required input and config data

--- openeis/algorithm/base.py
+++ openeis/algorithm/base.py
@@ -3,13 +3,45 @@ Created on Apr 23, 2014
 
 '''
 from abc import ABCMeta,abstractmethod
+from schema.schema import sensordata
+
+class InputDescriptor:
+    
+    def __init__(self,
+                 sensor_type,
+                 desc,
+                 minimum=1,
+                 maximum=1):
+        #TODO: check and throw exception if self.sensor_data is none
+        self.sensor_data = sensordata.get(sensor_type)
+        self.desc = desc
+        self.minimum = minimum
+        if(maximum is not None and minimum > maximum):
+            maximum = minimum
+        self.maximum = maximum
+        
 
 class DriverBaseClass(metaclass=ABCMeta):
     
     @classmethod
     @abstractmethod
     def required_input(cls):
-        "required input schema"
+        """required input schema
+                {
+                    'key1':InputDescriptor1,
+                    'key2':InputDescriptor2
+                }
+             e.g.: 
+                # OAT1 returns a list of 1 OAT
+                # OAT2 returns a list of 3 OATs
+                # CFP1 returns a list of minimum 3 and maximum all CFP1
+                
+                {
+                    'OAT1':InputDescriptor('OutdoorAirTemperature','Hillside OAT'),
+                    'OAT2':InputDescriptor('OutdoorAirTemperature','Roof OATs',minimum=3)
+                    'CFP1':InputDescriptor('CondenserFanPower','Roof OATs',minimum=3,maximum=None)
+                }
+        """
     
     @classmethod
     @abstractmethod
@@ -23,7 +55,19 @@ class DriverBaseClass(metaclass=ABCMeta):
     @classmethod
     @abstractmethod
     def get_config_parameters(cls):
-        "config schema description"
+        """default config schema description
+            {
+                'Key1':(Type1,DefaultValue1),
+                'Key2':(Type2,DefaultValue2)
+            }
+            
+            e.g.: 
+            {
+                'matemp_missing': (int,0),
+                'mat_low': (float, 50.4)
+                'output_label': (str, None)
+            }
+        """
     
     @abstractmethod
     def run(self,inp,out):
