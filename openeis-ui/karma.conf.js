a6486f1c883ef4e87f6c994aeb8c75d0ae23c363
Tim Kang <tim.kang@pnnl.gov>
2014-04-30 15:25:15 -0700

    Update deps and configs for CI

--- openeis-ui/karma.conf.js
+++ openeis-ui/karma.conf.js
@@ -1,12 +1,15 @@
 module.exports = function(config) {
   config.set({
     frameworks: ['jasmine'],
-    browsers: ['Chrome', 'Firefox', 'PhantomJS'],
+    browsers: ['PhantomJS'],
     files: [
       'bower_components/angular/angular.js',
       'bower_components/angular-*/angular-*.js',
       'bower_components/ng-file-upload/angular-file-upload.js',
       'js/*.js',
     ],
+    junitReporter: {
+      outputFile: 'ui-tests-junit-results.xml',
+    },
   });
 };
