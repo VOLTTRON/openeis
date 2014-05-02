94a3d504aaef94b728df9f950b4ba493296362d9
Kyle Monson <kyle.monson@pnnl.gov>
2014-05-02 12:39:14 -0700

    Word on db output.

--- openeis/database/db_output.py
+++ openeis/database/db_output.py
@@ -8,16 +8,30 @@ TODO: import database Table object and TableColumn object from Django database m
 class DatabaseOutput:
     
     def __init__(self,required_output):
+        self.table_map = {}
+        
         for (name,columns) in required_output.iter_items():
             self.create_table(name,columns)
              
     def create_table(self,name, columns):
-        pass
-#         t = Table(name=name)
-#         column_models = {}
-#         for col,type in columns.iter_items():
-#             col_data = TableColumns(table=t, name=col, type=type)
-#             column_models[col] = col_data      
+        t = Table(name=name)
+        column_info = {}
+        for col,type in columns.iter_items():
+            col_meta = TableColumns(table=t, name=col, type=type)
+            column_info[col] = col_meta
+        self.table_map[name] = column_info
          
     def insert_row(self,table_name,row_data):
-        pass
+        column_models = self.table_map.get(table_name)
+        
+        if column_models is None:
+            #TODO: report an error.
+            return
+        
+        for column_name, value in row_data.iter_items():
+            column_model = column_models.get(column_name)
+            if column_model is None:
+                #TODO :report an error
+                continue
+            
+            #table
