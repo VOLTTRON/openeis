a393fec3c0d07891ea504831d291d89f7393921c
Craig Allwardt <craig.allwardt@pnnl.gov>
2014-05-15 19:15:53 -0700

    moved this to senserstore.py file.

--- openeis/projects/storage/__init__.py
+++ openeis/projects/storage/__init__.py
@@ -1,19 +1,4 @@
-'''
-Created on May 14, 2014
 
-This module will contain the api associated with the application/algorithm storing
-and retrieving data.
-'''
-
-def get_sensors(project_id, topic, *kwarg):
-    '''
-    Get sensors returns a list of two-tuples.   The first element in the
-    tuple will be a meta object that will hold the mapping definition and
-    the sensor definition.  The second element will be a function that will
-    take no arguments and will return a new queryset.  The queryset will hold
-    two columns of data, the time and the data point value.  
-    '''
-    pass
     
     
                 
