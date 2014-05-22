2e28ccb700a2a297a42aa09c5ce0c245931b203e
Brandon Carpenter <brandon.carpenter@pnnl.gov>
2014-05-22 12:53:14 -0700

    Modified sensor data queryset and manager.
    Allow date truncation and grouping. Also fixed choices lists ordering.

--- openeis/projects/models.py
+++ openeis/projects/models.py
@@ -4,7 +4,7 @@ import random
 import string
 
 from django.contrib.auth.models import User
-from django.db import models
+from django.db import connections, models
 from django.db.models.query import QuerySet
 from django.core.exceptions import ValidationError
 
@@ -143,35 +143,6 @@ class SensorMapDefinition(models.Model):
                                 for name in path)): value
                                for path, value in errors.items()})
 
-class TableDataQuerySet(QuerySet):
-    sqlite_querys = {
-                     "minute":"""strftime('%%Y-%%m-%%d %%H:%%M', time_stamp)""",
-                     "hour":"""strftime('%%Y-%%m-%%d %%H', time_stamp)""",
-                     "day":"""strftime('%%Y-%%m-%%d', time_stamp)""",
-                     #"week":"""strftime('%%Y-%%m %W', time_stamp)""",
-                     "month":"""strftime('%%Y-%%m', time_stamp)""",
-                     "year":"""strftime('%%Y', time_stamp)""",
-                     }
-    
-    is_sqlite = False
-    
-    def group_by(self, period, func):  
-        if period not in self.sqlite_querys:
-            raise ValueError('Invalid Period for grouping.')   
-        if self.is_sqlite:
-            select_data = {'d':self.sqlite_querys[period]}
-        else:
-            #Relax people... We validate the period above.
-            select_data = {'d':"""date_trunc({0},time_stamp)""".format(period)}
-            
-        q = self.extra(select = select_data).values('d').annotate(aggregated_value=func('values'))
-        return q
-
-class TableDataManager(models.Manager):
-    def get_query_set(self):
-        return TableDataQuerySet(self.model)
-    def __getattr__(self, name):
-        return getattr(self.get_query_set(), name)
 
 class SensorIngest(models.Model):
     map = models.ForeignKey(SensorMapDefinition, related_name='ingests')
@@ -186,12 +157,15 @@ class SensorIngestFile(models.Model):
     name = models.CharField(max_length=255)
     file = models.ForeignKey(DataFile, related_name='ingests')
 
+    class Meta:
+        unique_together = ('ingest', 'name')
+
 
 class SensorIngestLog(models.Model):
     INFO = 20
     WARNING = 30
     ERROR = 40
-    LOG_LEVEL_CHOICES = (('Info', INFO), ('Warning', WARNING), ('Error', ERROR))
+    LOG_LEVEL_CHOICES = ((INFO, 'Info'), (WARNING, 'Warning'), (ERROR, 'Error'))
 
     ingest = models.ForeignKey(SensorIngestFile, related_name='logs')
     row = models.IntegerField()
@@ -207,17 +181,106 @@ class Sensor(models.Model):
     INTEGER = 'i'
     STRING = 's'
 
-    DATA_TYPE_CHOICES = (('boolean', BOOLEAN), ('float', FLOAT),
-                         ('integer', INTEGER), ('string', STRING))
+    DATA_TYPE_CHOICES = ((BOOLEAN, 'boolean'), (FLOAT, 'float'),
+                         (INTEGER, 'integer'), (STRING, 'string'))
 
     map = models.ForeignKey(SensorMapDefinition, related_name='sensors')
     # name matches the sensor path in the definition
     name = models.CharField(max_length=255)
     data_type = models.CharField(max_length=1, choices=DATA_TYPE_CHOICES)
 
+    class Meta:
+        unique_together = ('map', 'name')
+
     @property
     def data(self):
-        return getattr(self, self.data_type + 'sensordata_set')
+        return getattr(self, self.get_data_type_display() + 'sensordata_set')
+
+
+class SensorDataQuerySet(QuerySet):
+    class pg_trunc(dict):
+        def __missing__(self, key):
+            if key not in {'minute', 'hour', 'day', 'month', 'year'}:
+                raise KeyError(key)
+            return 'date_trunc({0}, {{field}})'.format(key)
+
+    trunc_funcs = {
+        "mysql": {
+            "year": "strftime('%%Y', {field})",
+            "month": "strftime('%%Y-%%m', {field})",
+            "day": "strftime('%%Y-%%m-%%d', {field})",
+            "hour": "strftime('%%Y-%%m-%%d %%H', {field})",
+            "minute": "strftime('%%Y-%%m-%%d %%H:%%i', {field})",
+        },
+        "oracle": {
+            "year": "trunc({field}, 'YEAR')",
+            "month": "trunc({field}, 'MONTH')",
+            "day": "trunc({field}, 'DAY')",
+            "hour": "trunc({field}, 'HH24')",
+            "minute": "trunc({field}, 'MI')",
+        },
+        "postgresql": pg_trunc(),
+        "sqlite": {
+            "year": "strftime('%%Y', {field})",
+            "month": "strftime('%%Y-%%m', {field})",
+            "day": "strftime('%%Y-%%m-%%d', {field})",
+            "hour": "strftime('%%Y-%%m-%%d %%H', {field})",
+            "minute": "strftime('%%Y-%%m-%%d %%H:%%M', {field})",
+        },
+    }
+
+    def trunc_date(self, kind, *args, **kwargs):
+        '''Truncate a date field to the level indicated by kind.
+
+        kind must be one of 'year', 'month', 'day', 'hour', or 'minute'.
+        args and kwargs contain field names to truncate. Names in args
+        retain the same name while those in values of kwargs get named
+        according to the key.
+        '''
+        backend = connections[self.db].vendor
+        try:
+            trunc = self.trunc_funcs[backend]
+        except KeyError:
+            raise NotImplementedError('group_by is not implemented for the '
+                                      '{} database backend'.format(backend))
+        try:
+            func = trunc[kind]
+        except KeyError:
+            raise ValueError('invalid truncation kind: {}'.format(kind))
+        for arg in args:
+            if arg in kwargs:
+                raise ValueError('field given twice: {}'.format(arg))
+            kwargs[arg] = arg
+        select = {dest: func.format(field=source)
+                  for dest, source in kwargs.items()}
+        return self.extra(select=select) if select else self
+
+    def timeseries(self, *, trunc_kind=None, aggregate=None):
+        '''Return timeseries pairs from the table.
+
+        Returns 2-tuples of time-value pairs. If trunc_kind is given,
+        the time is truncated to the given precision. If aggregate is
+        given, the series values are aggregated according to the given
+        aggregation method and grouped by the time.
+        '''
+        queryset = self
+        if trunc_kind:
+            queryset = queryset.trunc_date(trunc_kind, 'time')
+        if aggregate:
+            queryset = queryset.values('time').annotate(value=aggregate('value'))
+        return queryset.values_list('time', 'value')
+
+
+class SensorDataManager(models.Manager):
+    def get_queryset(self):
+        return SensorDataQuerySet(self.model)
+    @property
+    def timeseries(self):
+        return self.get_queryset().timeseries
+    @property
+    def trunc_date(self):
+        return self.get_queryset().trunc_date
+
 
 
 class BaseSensorData(models.Model):
@@ -227,16 +290,22 @@ class BaseSensorData(models.Model):
 
     class Meta:
         abstract = True
+        ordering = ['time']
+        get_latest_by = 'time'
 
 
 class BooleanSensorData(BaseSensorData):
     value = models.NullBooleanField()
+    objects = SensorDataManager()
 
 class FloatSensorData(BaseSensorData):
     value = models.FloatField(null=True)
+    objects = SensorDataManager()
 
 class IntegerSensorData(BaseSensorData):
     value = models.IntegerField(null=True)
+    objects = SensorDataManager()
 
 class StringSensorData(BaseSensorData):
     value = models.TextField(null=True)
+    objects = SensorDataManager()
