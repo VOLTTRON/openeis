b04c0d13382a7ca57c21db3ef7e4683a05c93514
Craig Allwardt <craig.allwardt@pnnl.gov>
2014-05-22 20:43:08 -0700

    Attempting fixing of fixtures.

--- openeis/runtests.bat
+++ openeis/runtests.bat
@@ -3,4 +3,6 @@ REM this should probably be a test server settings but for not
 REM use the default settings.
 
 set DJANGO_SETTINGS_MODULE=openeis.server.settings
+rem mv projects\fixtures\initial_data.json projects\fixtures\initial_data.json.bak
 nosetests --with-xunit -v
+rem mv projects\fixtures\initial_data.json.bak projects\fixtures\initial_data.json
