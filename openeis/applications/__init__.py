c2eb6cbb05c522a4d8e651125bb586f4fa466884
David M. Lorenzetti <dmlorenzetti@lbl.gov>
2014-07-01 14:39:42 -0700

    Suppressed diagnostic output when done building application dictionary.

--- openeis/applications/__init__.py
+++ openeis/applications/__init__.py
@@ -12,7 +12,7 @@ _applicationList = [name for _, name, _ in pkgutil.iter_modules(__path__)]
 _applicationDict = {}
 
 class InputDescriptor:
-    
+
     def __init__(self,
                  sensor_type,
                  desc,
@@ -30,9 +30,9 @@ class InputDescriptor:
         if(count_max is not None and count > count_max):
             count_max = count
         self.count_max = count_max
-        
+
 class OutputDescriptor:
-    
+
     def __init__(self,
                  output_type,
                  topic):
@@ -40,7 +40,7 @@ class OutputDescriptor:
         #self.output_type = sensordata.get(sensor_type)
         self.output_type = output_type
         self.topic = topic
-        
+
 class ConfigDescriptor:
     def __init__(self,
                  config_type,
@@ -57,7 +57,7 @@ class ConfigDescriptor:
         self.value_max = value_max
 
 class DriverApplicationBaseClass(metaclass=ABCMeta):
-    
+
     def __init__(self,inp=None,out=None,**kwargs):
         """
         When applications extend this base class, they need to make
@@ -66,80 +66,80 @@ class DriverApplicationBaseClass(metaclass=ABCMeta):
         super().__init__(**kwargs)
         self.inp = inp
         self.out = out
-        
+
     @classmethod
     def single_file_input(cls):
         #change this to true if all input must come from the same file.
         return False
-    
+
     @classmethod
     @abstractmethod
     def required_input(cls):
         """
         Applications will override this method to return a dictionary specifying their
         data needs. This method will be called by the UI to do the mapping based on this.
-        
+
         required input schema
                 {
                     'key1':InputDescriptor1,
                     'key2':InputDescriptor2
                 }
-             e.g.: 
+             e.g.:
                 # OAT1 returns a list of 1 OAT
                 # OAT2 returns a list of 3 OATs
                 # CFP1 returns a list of minimum 3 and maximum all CFP1
-                
+
                 {
                     'OAT1':InputDescriptor('OutdoorAirTemperature','Hillside OAT'),
                     'OAT2':InputDescriptor('OutdoorAirTemperature','Roof OATs',count_min=3)
                     'CFP1':InputDescriptor('CondenserFanPower','CFP Desc',count_min=3,count_max=None)
                 }
         """
-    
+
     @classmethod
     @abstractmethod
     def output_format(cls, input_object):
         """
         The output object takes the resulting input object as a argument
          so that it may give correct topics to it's outputs if needed.
-         
+
         output schema description
            {TableName1: {name1:OutputDescriptor1, name2:OutputDescriptor2,...},....}
-           
-           eg: {'OAT': {'Timestamp':OutputDescriptor('timestamp', 'foo/bar/timestamp'),'OAT':OutputDescriptor('OutdoorAirTemperature', 'foo/bar/oat')}, 
-                'Sensor': {'SomeValue':OutputDescriptor('int', 'some_output/value'), 
+
+           eg: {'OAT': {'Timestamp':OutputDescriptor('timestamp', 'foo/bar/timestamp'),'OAT':OutputDescriptor('OutdoorAirTemperature', 'foo/bar/oat')},
+                'Sensor': {'SomeValue':OutputDescriptor('int', 'some_output/value'),
                            'SomeOtherValue':OutputDescriptor('boolean', 'some_output/value),
-                           'SomeString':OutputDescriptor('string', 'some_output/string)}} 
+                           'SomeString':OutputDescriptor('string', 'some_output/string)}}
         """
-        
+
     @classmethod
     @abstractmethod
     def get_config_parameters(cls):
-        """default config schema description used by the UI to get user input 
+        """default config schema description used by the UI to get user input
             which will be passed into the application
             Default values are used to prepopulate the UI, not to pass into the app by default
-            
+
             See ConfigDescriptor for all arguments.
             {
                 'Key1':ConfigDescriptor(Type1,Description1),
                 'Key2':ConfigDescriptor(Type2,Description2)
             }
-            
-            e.g.: 
+
+            e.g.:
             {
                 "building_sq_ft": ConfigDescriptor(float, "Square footage"),
                 "building_year_constructed": ConfigDescriptor(int, "Consruction Year"),
                 "building_name": ConfigDescriptor(str, "Building Name", optional=True)
             }
         """
-    
+
     @abstractmethod
     def execute(self):
         """
         Called when user says Go! in the UI
         """
         "The algorithm to run."
-        
+
     @abstractmethod
     def report(self):
         """describe output"""
@@ -149,25 +149,25 @@ class DrivenApplicationBaseClass(DriverApplicationBaseClass, metaclass=ABCMeta):
     @classmethod
     def single_file_input(cls):
         return True
-    
+
     def execute(self):
         '''Iterate over input calling run each time'''
         pass
-    
+
     @classmethod
     @abstractmethod
     def inputs(cls):
         return True
-    
+
     @abstractmethod
     def run(self, time, inputs):
         '''Do work for each batch of timestamped inputs
            time- current time
-           inputs - dict of point name -> value  
-           
+           inputs - dict of point name -> value
+
            Must return a results object.'''
         pass
-    
+
     def shutdown(self):
         '''Override this to add shutdown routines.'''
         return Results()
@@ -177,16 +177,16 @@ class Results:
         self.commands = {}
         self.log_messages = []
         self.terminate = terminate
-        
+
     def command(self, point, value):
         self.commands[point]=value
-    
+
     def log(self, message, level=logging.DEBUG):
         self.log_messages.append((level, message))
-        
+
     def terminate(self, terminate):
         self.terminate = bool(terminate)
-        
+
 for applicationName in _applicationList:
     try:
         absolute_app = '.'+applicationName
@@ -195,16 +195,16 @@ for applicationName in _applicationList:
     except Exception as e:
         logging.error('Module {name} cannot be imported. Reason: {ex}'.format(name=applicationName, ex=e))
         continue
-    
+
     #Validation of Algorithm class
-    
+
     if not issubclass(klass, DriverApplicationBaseClass):
         logging.warning('The implementation of {name} does not inherit from openeis.algorithm.DriverApplicationBaseClass.'.format(name=applicationName))
-     
-    _applicationDict[applicationName] = klass
-    
-print(_applicationDict)
 
-    
+    _applicationDict[applicationName] = klass
+
+# print(_applicationDict)
+
+
 def get_algorithm_class(name):
     return _applicationDict.get(name)
