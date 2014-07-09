0e12bd4fa9472779e81a331bf2c67c05f5bd8869
Poorva Sharma <poorva.sharma@pnnl.gov>
2014-07-09 10:29:33 -0700

    Added heat map and scatter plot

--- openeis/projects/storage/reports.py
+++ openeis/projects/storage/reports.py
@@ -32,7 +32,7 @@ class ReportElement:
                 continue
             value = getattr(self,key)
             if value is not None:
-                result += '\n\t' + key + ' = ' + str(value) + ', '
+                result += '\n\t' + key + ' = ' + str(value)
         return result
     
 class TextBlurb(ReportElement):
@@ -61,8 +61,6 @@ class XYDataSet():
         return result
     
 
-    
-            
 class XYPlot(ReportElement):
     
     def __init__(self, xy_dataset_list, x_label, y_label, title=None, desc=None):
@@ -75,29 +73,48 @@ class XYPlot(ReportElement):
 class LinePlot(XYPlot):
     pass
 
-#TODO
 class BarChart(XYPlot):
     pass
 
-class HeatMap(XYPlot):
+class ScatterPlot(XYPlot):
     pass
+
+class HeatMap(ReportElement):
+    
+    def __init__(self, table_name, x_column, y_column, z_column, x_label=None, y_label=None, z_label=None, title=None, desc=None):
+        super(HeatMap, self).__init__(title, desc)
+        self.table_name = table_name
+        self.x_column  = x_column
+        self.y_column = y_column
+        self.z_column = z_column
+        self.x_label = x_label
+        self.y_label = y_label
+        self.z_label = z_label
 
 
 if __name__ == '__main__':
     
     report = Report("This is a report description")
     
-    #line_plot = LinePlot(title='Title', table_name="OAT", x_column="Temperature", y_column="Timestamp")
     table = Table(table_name="XYZ")
+    report.add_element(table)
+    
     text_blurb = TextBlurb(text="this is a text blurb")
+    report.add_element(text_blurb)
     
     xy_dataset_list = []
     xy_dataset_list.append(XYDataSet('OAT','Timestamp','Temperature'))
     xy_dataset_list.append(XYDataSet('Sensor','Timestamp','SensorValue'))
-    xy_plot = LinePlot(xy_dataset_list, title='line chart', x_label='Temperature', y_label='values')
+    line_plot = LinePlot(xy_dataset_list, title='line chart', x_label='Temperature', y_label='values')
+    report.add_element(line_plot)
     
-    report.add_element(table)
-    report.add_element(text_blurb)
-    report.add_element(xy_plot)
+    bar_chart = BarChart(xy_dataset_list, title='bar chart', x_label='Temperature', y_label='values')
+    report.add_element(bar_chart)
+    
+    scatter_plot = ScatterPlot(xy_dataset_list, title='scatter plot', x_label='Temperature', y_label='values')
+    report.add_element(scatter_plot)
+    
+    heat_map = HeatMap(table_name='OAT_HeatMap', x_column='HourOfDay', y_column='Date', z_column='Temperature')
+    report.add_element(heat_map)
     
     print(report)
