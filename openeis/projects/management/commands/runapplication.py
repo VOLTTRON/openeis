a7649b31af1d44d78e322cef2509f2afbd198b6d
Kyle Monson <kyle.monson@pnnl.gov>
2014-07-31 17:14:53 -0700

    Update runapplication to create and save analyses to db.

--- openeis/projects/management/commands/runapplication.py
+++ openeis/projects/management/commands/runapplication.py
@@ -6,6 +6,10 @@ from openeis.projects.storage.db_output import DatabaseOutputFile
 from openeis.projects.storage.db_input import DatabaseInput
 
 from openeis.applications import get_algorithm_class
+from openeis.projects import serializers
+
+from datetime import datetime
+from django.utils.timezone import utc
 
 from configparser import ConfigParser
 
@@ -23,52 +27,75 @@ class Command(BaseCommand):
         # Put of importing modules that access the database to allow
         # Django to magically install the plumbing first.
         from openeis.projects.storage import sensorstore
-
-        verbosity = int(verbosity)
-
-        config = ConfigParser()
-
-        config.read(args[0])
-
-        application = config['global_settings']['application']
-        klass = get_algorithm_class(application)
-
-        dataset_ids = None
-        if config.has_option('global_settings', 'dataset_id'):
-            dataset_id_string = config['global_settings']['dataset_id']
-            dataset_ids = [int(x) for x in dataset_id_string.split(',')]
-
-        sensormap_id = int(config['global_settings']['sensormap_id'])
-        topic_map = {}
-
-        inputs = config['inputs']
-        for group, topics in inputs.items():
-            topic_map[group] = topics.split()
-
-
-        db_input = DatabaseInput(sensormap_id, topic_map, dataset_ids=dataset_ids)
-
-        output_format = klass.output_format(db_input)
-        file_output = DatabaseOutputFile(application, output_format)
-
-        kwargs = {}
-        if config.has_section('application_config'):
-            for arg, str_val in config['application_config'].items():
-                kwargs[arg] = eval(str_val)
-
-        if( verbosity > 1 ):
-            print('Running application:', application)
-            print('- Sensor map id:', sensormap_id)
-            if dataset_ids is not None:
-                print('- Data set ids:', dataset_ids)
-            print('- Topic map:', topic_map)
-            print('- Output format:', output_format)
-
-        app = klass(db_input, file_output, **kwargs)
-        app.run_application()
-
-        reports = klass.reports(output_format)
-
-        for report in reports:
-            print(report)
-
+        from openeis.projects import models
+        
+        
+        
+        try:
+            verbosity = int(verbosity)
+    
+            config = ConfigParser()
+    
+            config.read(args[0])
+    
+            application = config['global_settings']['application']
+            klass = get_algorithm_class(application)
+    
+            dataset_ids = None
+            if config.has_option('global_settings', 'dataset_id'):
+                dataset_id_string = config['global_settings']['dataset_id']
+                dataset_ids = [int(x) for x in dataset_id_string.split(',')]
+    
+            sensormap_id = int(config['global_settings']['sensormap_id'])
+            
+            now = datetime.utcnow().replace(tzinfo=utc)
+            analysis = models.Analysis(added=now, started=now, status="running",
+                                       dataset_id=sensormap_id)
+            analysis.save()
+            
+            topic_map = {}
+    
+            inputs = config['inputs']
+            for group, topics in inputs.items():
+                topic_map[group] = topics.split()
+    
+    
+            db_input = DatabaseInput(sensormap_id, topic_map, dataset_ids=dataset_ids)
+    
+            output_format = klass.output_format(db_input)
+            file_output = DatabaseOutputFile(application, output_format)
+    
+            kwargs = {}
+            if config.has_section('application_config'):
+                for arg, str_val in config['application_config'].items():
+                    kwargs[arg] = eval(str_val)
+    
+            if( verbosity > 1 ):
+                print('Running application:', application)
+                print('- Sensor map id:', sensormap_id)
+                if dataset_ids is not None:
+                    print('- Data set ids:', dataset_ids)
+                print('- Topic map:', topic_map)
+                print('- Output format:', output_format)
+    
+            app = klass(db_input, file_output, **kwargs)
+            app.run_application()
+    
+            reports = klass.reports(output_format)
+    
+            for report in reports:
+                print(report)
+        
+        except Exception as e:
+            analysis.status = "error"
+            # TODO: log errors
+            print(e)
+    
+        finally:
+            if analysis.status != "error":
+                analysis.reports = [serializers.ReportSerializer(report).data for
+                                    report in klass.reports(file_output)]
+                analysis.status = "complete"
+                analysis.progress_percent = 100
+            analysis.ended = datetime.utcnow().replace(tzinfo=utc)
+            analysis.save()
