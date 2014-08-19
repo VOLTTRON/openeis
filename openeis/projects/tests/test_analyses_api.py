4a8d557f2e04e80a6cc9260291d298ada1890c0e
Jereme Haack <Jereme.Haack@pnnl.gov>
2014-08-19 16:18:24 -0700

    Added another app to the test

--- openeis/projects/tests/test_analyses_api.py
+++ openeis/projects/tests/test_analyses_api.py
@@ -1,4 +1,4 @@
-'''Tests for RESTful API.
+'''Tests for RESTful Analyses API.
 '''
 
 from datetime import datetime, timezone
@@ -17,9 +17,9 @@ from openeis.projects.tests.openeistest import OpenEISTestBase
 
 
 class TestAnalysesApi(OpenEISTestBase):
-    fixtures = ['analyses_test_data.json']
+    fixtures = ['db_dump_test_userPWtest_project.json','analyses_test_data.json']
 
-    def test_application_run(self):
+    def test_daily_summary_run(self):
         # TODO Add test for running an application
         from openeis.applications import (_applicationDict)
 
@@ -61,3 +61,58 @@ class TestAnalysesApi(OpenEISTestBase):
         analysis_id = response.data['id']
         self.assertIsNotNone(analysis_id)
         self.assertTrue(analysis_id > 0)
+
+
+#        time.sleep(30)
+#        response = client.get('/api/analyses')
+#        self.assertEqual(status.HTTP_200_OK, response.status_code)
+#        self.assertEqual(response.data[0]['status'], "completed")
+
+    def test_energy_signature_run(self):
+        # TODO Add test for running an application
+        from openeis.applications import (_applicationDict)
+
+        app_name = 'energy_signature'
+        self.assertIn(app_name, _applicationDict, app_name + " not in application list")
+
+
+
+        client = APIClient()
+        response = client.get('/api/analyses')
+        self.assertEqual(status.HTTP_403_FORBIDDEN, response.status_code)
+
+        client = self.get_authenticated_client()
+
+        #Make sure the applicatin we're looking for exists
+
+        response = client.get('/api/analyses')
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+
+        post_data = json.dumps(    {
+            "name": "EnergySignature - Unit Test",
+            "dataset": 1,
+            "application": "energy_signature",
+            "configuration": {
+                "inputs": {
+                    "oat": [
+                        "lbnl/bldg90/OutdoorAirTemperature"
+                    ],
+                    "load": [
+                        "lbnl/bldg90/WholeBuildingElectricity"
+                    ]
+                },
+                "parameters": {}
+            }
+        })
+        response = client.post('/api/analyses', post_data, content_type='application/json', Accept='application/json')
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
+        analysis_id = response.data['id']
+        self.assertIsNotNone(analysis_id)
+        self.assertTrue(analysis_id > 0)
+
+#        time.sleep(30)
+#        response = client.get('/api/analyses')
+#        self.assertEqual(status.HTTP_200_OK, response.status_code)
+#        self.assertEqual(response.data[0]['status'], "completed")
+
+
