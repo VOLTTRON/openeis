f97206cb9c5c9b952c570d7997db2e54b3de1f30
Jereme Haack <Jereme.Haack@pnnl.gov>
2014-09-09 13:51:26 -0700

    Cleaned up file

--- openeis/applications/interpolation_example.py
+++ openeis/applications/interpolation_example.py
@@ -1,3 +1,58 @@
+# -*- coding: utf-8 -*- {{{
+# vim: set fenc=utf-8 ft=python sw=4 ts=4 sts=4 et:
+#
+# Copyright (c) 2014, Battelle Memorial Institute
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# The views and conclusions contained in the software and documentation are those
+# of the authors and should not be interpreted as representing official policies,
+# either expressed or implied, of the FreeBSD Project.
+#
+#
+# This material was prepared as an account of work sponsored by an
+# agency of the United States Government.  Neither the United States
+# Government nor the United States Department of Energy, nor Battelle,
+# nor any of their employees, nor any jurisdiction or organization
+# that has cooperated in the development of these materials, makes
+# any warranty, express or implied, or assumes any legal liability
+# or responsibility for the accuracy, completeness, or usefulness or
+# any information, apparatus, product, software, or process disclosed,
+# or represents that its use would not infringe privately owned rights.
+#
+# Reference herein to any specific commercial product, process, or
+# service by trade name, trademark, manufacturer, or otherwise does
+# not necessarily constitute or imply its endorsement, recommendation,
+# or favoring by the United States Government or any agency thereof,
+# or Battelle Memorial Institute. The views and opinions of authors
+# expressed herein do not necessarily state or reflect those of the
+# United States Government or any agency thereof.
+#
+# PACIFIC NORTHWEST NATIONAL LABORATORY
+# operated by BATTELLE for the UNITED STATES DEPARTMENT OF ENERGY
+# under Contract DE-AC05-76RL01830
+#
+#}}}
+
 from openeis.applications import DriverApplicationBaseClass, InputDescriptor, OutputDescriptor, ConfigDescriptor
 from openeis.projects.storage.db_output import DatabaseOutputFile
 import logging
@@ -18,7 +73,7 @@ from openeis.projects.storage import db_output
 
 class Application(DriverApplicationBaseClass):
 
-    def __init__(self,*args,building_sq_ft=-1, building_year_constructed=-1, building_name=None,**kwargs):
+    def __init__(self,*args,**kwargs):
         #Called after app has been staged
         """
         When applications extend this base class, they need to make
@@ -26,29 +81,10 @@ class Application(DriverApplicationBaseClass):
         """
         super().__init__(*args,**kwargs)
 
-#        self.default_building_name_used = False
-#
-#        if building_sq_ft < 0:
-#            raise Exception("Invalid input for building_sq_ft")
-#        if building_year_constructed < 0:
-#            raise Exception("Invalid input for building_sq_ft")
-#        if building_name is None:
-#            building_name = "None supplied"
-#            self.default_building_name_used = True
-#
-#        self.sq_ft = building_sq_ft
-#        self.building_year = building_year_constructed
-#        self.building_name = building_name
-
-
-
     @classmethod
     def get_config_parameters(cls):
         #Called by UI
         return {
-#                    "building_sq_ft": ConfigDescriptor(float, "Square footage", value_min=200),
-#                    "building_year_constructed": ConfigDescriptor(int, "Consruction Year", value_min=1800, value_max=2014),
-#                    "building_name": ConfigDescriptor(str, "Building Name", optional=True)
 
                 }
 
@@ -59,15 +95,12 @@ class Application(DriverApplicationBaseClass):
         return {
                     'oat':InputDescriptor('OutdoorAirTemperature','Outdoor Temp', count_min=1,count_max=10),
                     'load':InputDescriptor('WholeBuildingElectricity','Building Load'),
-#                    'natgas':InputDescriptor('WholeBuildingGas','Natural Gas usage')
                 }
 
     @classmethod
     def output_format(cls, input_object):
         #Called when app is staged
         topics = input_object.get_topics()
-        # Work with topics["OAT"][0] to get building topic
-#         'SomeString':OutputDescriptor('string', 'some_output/string)}
         oats = topics['oat']
 
         output_needs =  {'Time_Table':
@@ -95,19 +128,8 @@ class Application(DriverApplicationBaseClass):
         report = reports.Report(rep_desc)
 
         column_info = (('time', 'Timestamp'), ('load', 'Load'))
-#
-        print(output_object)
-        print(output_object.__class__.__name__)
-#        print(output_object.output_names)
-#        print(output_object.output_names['Time_Table'])
-        keys = None
 
-        if isinstance(output_object, dict):
-            keys = output_object['Time_Table'].keys()
-        elif isinstance(output_object, DatabaseOutputFile):
-            keys = list(output_object.output_names['Time_Table'])
-
-#        print(keys)
+        keys = output_object['Time_Table'].keys()
 
         for key in keys:
             if key.startswith('oat'):
@@ -129,8 +151,6 @@ class Application(DriverApplicationBaseClass):
         return report_list
 
     def execute(self):
-        #Called after User hits GO
-        "Do stuff"
         self.out.log("Starting analysis", logging.INFO)
 
         load = self.inp.get_query_sets('load',wrap_for_merge=True)
@@ -150,109 +170,4 @@ class Application(DriverApplicationBaseClass):
                 row_to_write[name] = oats[count]
                 count += 1
             self.out.insert_row("Time_Table", row_to_write)
-
-
-        #Go through some data
-#         data_start, data_end = self.inp.get_start_end_times()
-
-        #A year ago ignoring time info
-#         year_ago = (data_end - relativedelta(year=1)).replace(hour=0,minute=0,second=0)
-#
-#         #A month ago ignoring time info
-#         month_ago = (data_end - relativedelta(month=1)).replace(hour=0,minute=0,second=0)
-
-        #
-
-
-        #To be used for generating an energy signature plot
-#         oat_year_by_day = self.inp.group_by('OAT',year_ago, data_end, "day")
-#         load_year_by_day = self.inp.group_by('load',year_ago, data_end, "day")
-#         natgas_year_by_day = self.inp.group_by('natgas',year_ago, data_end, "day")
-#
-#         oat_month_by_day = self.inp.group_by('OAT',month_ago, data_end, "day")
-#         load_month_by_day = self.inp.group_by('load',month_ago, data_end, "day")
-#         natgas_month_by_day = self.inp.group_by('natgas',month_ago, data_end, "day")
-
-#        print (self.inp.get_topics())
-#
-#        print (self.inp.get_topics_meta()['load']['pnnl/isb1/WholeBuildingElectricity']['unit'])
-#        print (self.inp.get_topics_meta()['load'][self.inp.get_topics()['load'][0]]['unit'])
-
-
-
-
-#        load_max = self.inp.get_query_sets('load',group_by='all',group_by_aggregation=Max)[0]
-#        load_min = self.inp.get_query_sets('load',group_by='all',group_by_aggregation=Min)[0]
-#
-#        # gather loads and outside air temperatures. Reduced to and hourly average
-#        load_query = self.inp.get_query_sets('load', group_by='hour',group_by_aggregation=Avg,
-#                                             exclude={'value':None},
-#                                             wrap_for_merge=True)
-#        oat_query = self.inp.get_query_sets('oat', group_by='hour',group_by_aggregation=Avg,
-#                                             exclude={'value':None},
-#                                             wrap_for_merge=True)
-#
-#
-##         merged_load_oat = self.inp.merge(load_query,oat_query, drop_partial_lines=True)
-##         month_filter ={'time__gte':month_ago}
-#
-#        thing = self.inp.get_query_sets('load',group_by='hour',
-#                                                    group_by_aggregation=Sum)
-#
-#        load_by_hour = self.inp.get_query_sets('load',group_by='hour',
-#                                                    group_by_aggregation=Sum)[0]
-#        by_hour = load_by_hour.filter(time__hour=1)
-#
-#
-#        #std_dev_load_by_hour  = load_by_hour.filter(time__hour=1).aggregate(value=Sum('values'))
-#        #load_by_hour.filter(time__hour=1).timeseries(aggregate=StdDev)
-#
-#
-#
-##         print(std_dev_load_by_hour)
-#        print(load_min)
-#        print(load_max)
-#
-#        print(self.inp.get_topics_meta())
-#
-#
-#
-#
-#        #loads by day for dailysummary stats
-#        #peak95
-#        #mbase5
-#        #bpratio
-#        #range
-##         self.out.insert_row("Analysis_Table", {"Metric": "Load Max", "value": str(load_max)})
-##         self.out.insert_row("Analysis_Table", {"Metric": "Load Min", "value": str(load_min)})
-##         self.out.insert_row("Analysis_Table", {"Metric": "Load StdDev", "value": str(django.StdDev(load_month_by_day))})
-##         self.out.insert_row("Analysis_Table", {"Metric": "Load Mean", "value": str(django.Avg(load_month_by_day))})
-##         self.out.insert_row("Analysis_Table", {"Metric": "Load Variance", "value": str(django.Variance(load_month_by_day))})
-##
-#
-#        #Setup heat map
-#
-#
-##         success = gr_bldg.genEnergySignaturePlot(oatsCurrYear, loadsCurrYear,
-##             bldgMetaData['oat-units'], bldgMetaData['load-units'],
-##             figWritePath=os.path.join(outDirName,figRelPath))
-#
-##         for
-##         self.out.insert_row("HeatMap", {"Times by Day": thing, "Loads by Day": str(django.Max(load_month_by_day))})
-#
-#
-#
-#
-#
-#
-#
-##
-##         oat_sum = self.inp.group_by('OAT',data_start, data_end, "hour")
-##         load_sum = self.inp.group_by('laod',data_start, data_end, "hour")
-##         natgas_sum = self.inp.group_by('natgas',data_start, data_end, "hour")
-##
-##         merged_group = self.inp.merge(oat_sum, load_sum, natgas_sum)
-##
-
-
 
