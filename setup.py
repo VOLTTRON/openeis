4ef6f22f2574ec02cf9ba3d7634d270002c2f5b8
Craig Allwardt <craig.allwardt@pnnl.gov>
2014-09-11 14:07:48 -0700

    Added py.test to run tests for tests throughout the project.  py.test is the command to run whilest the pytest.ini is the command to run all the tests in our project

--- setup.py
+++ setup.py
@@ -64,11 +64,12 @@ install_requires = [
         #'django-guardian',
         'djangorestframework>=2.3,<2.4',
         'django-rest-swagger>=0.1,<0.2',
-        'django-nose',
+        #'django-nose',
         'jsonschema',
         'openeis-ui>0.1.dev70',
         'pytest-django',
         'pytz',
+        'pytest-django'
 ]
 
 basepath = os.path.dirname(os.path.abspath(__file__))
