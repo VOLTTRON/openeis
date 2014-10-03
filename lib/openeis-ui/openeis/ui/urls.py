c79ccf8275c4fd868645d6153fe0ca85cea04be0
Tim Kang <tim.kang@pnnl.gov>
2014-10-02 17:10:24 -0700

    Update UI to openeis-ui@78f10b9

--- lib/openeis-ui/openeis/ui/urls.py
+++ lib/openeis-ui/openeis/ui/urls.py
@@ -67,9 +67,9 @@ UI_STATIC_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__),
                                              'static/openeis-ui'))
 
 urlpatterns = [
-    url(r'^(?P<path>settings\.js|sensormap-schema\.json)$', 'django.views.static.serve',
+    url(r'^(?P<path>settings\.js)$', 'django.views.static.serve',
         {'document_root': UI_STATIC_DIR}),
-    url(r'^(?P<path>(?:css|js)/.+)$', 'django.views.static.serve',
+    url(r'^(?P<path>(?:css|fonts|js)/.+)$', 'django.views.static.serve',
         {'document_root': UI_STATIC_DIR}),
     url(r'', 'django.views.static.serve',
         {'path': 'index.html', 'document_root': UI_STATIC_DIR}),
