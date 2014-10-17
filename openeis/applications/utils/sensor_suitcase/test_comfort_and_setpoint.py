c89e9acac4e45d3033b051f360000087afc9cc0b
Vivian Nguyen <vtnguyen@lbl.gov>
2014-10-17 10:27:26 -0700

    [Sensor Suitcase] renamed tests from utest to test for py.test to find

--- openeis/applications/utils/sensor_suitcase/test_comfort_and_setpoint.py
+++ openeis/applications/utils/sensor_suitcase/test_comfort_and_setpoint.py
@@ -73,7 +73,33 @@ class TestComfortAndSetback(AppTestBase):
 
         op_hours = [[0, 23], [1, 2, 3, 4, 5, 6, 7], []]
 
+        comfort, setback = comfort_and_setpoint(IAT, DAT, op_hours, 5000, 1000)
+
+        print (setback)
+
+        self.assertIsNot(comfort, False)
+        self.assertTrue(setback)
+"""
+negatives, all zeros
+    def test_economizer_empty(self):
+        a = datetime.datetime(2014, 1, 1, 0, 0, 0, 0)
+        b = datetime.datetime(2014, 1, 4, 0, 0, 0, 0)
+        #delta = 6 hours
+        base = set_up_datetimes(a, b, 21600)
+
+        DAT = copy.deepcopy(base)
+        DAT_temp = [8, 8, 8, 8, 8, 8, 8, 8, 100, 100, 100, 100, 100]
+        append_data_to_datetime(DAT, DAT_temp)
+
+        IAT = copy.deepcopy(base)
+        IAT_temp = [10, 10, 10, 10, 10, 10, 10, 10, 80, 80, 80, 80, 80]
+        append_data_to_datetime(IAT, IAT_temp)
+
+        op_hours = [[0, 23], [1, 2, 3, 4, 5, 6, 7], []]
+
         comfort, setback = comfort_and_setpoint(IAT, DAT, op_hours)
 
         self.assertIsNot(comfort, False)
         self.assertTrue(setback)
+"""
+
