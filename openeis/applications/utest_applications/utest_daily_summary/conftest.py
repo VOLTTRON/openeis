813be08ffa9166c12aaa88548659bb674e8c1dd1
Craig Allwardt <craig.allwardt@pnnl.gov>
2014-11-05 12:45:55 -0800

    Updated for daily summary rather than energy.

--- openeis/applications/utest_applications/utest_daily_summary/conftest.py
+++ openeis/applications/utest_applications/utest_daily_summary/conftest.py
@@ -9,6 +9,6 @@ abs_file_dir = os.path.join(os.path.dirname(__file__))
 
 @pytest.fixture
 def daily_summary_datafile(project):
-    return create_data_file(os.path.join(abs_file_dir, 'energy_signature_data.csv'), 
+    return create_data_file(os.path.join(abs_file_dir, 'daily_summary_data.csv'), 
                             project=project,
-                            comments='Energy signature test data data.')
+                            comments='Daily Summary test data.')
