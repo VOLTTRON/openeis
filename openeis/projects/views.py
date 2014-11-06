VOLTTRON/openeis

commit 245613f3d289deac4aa300d89e8283e28f20966b

Save analysis status in memory instead of the database.
Fixes EIS-470.

openeis/projects/views.py
@@ -487,7 +487,7 @@ class DataMapViewSet(viewsets.ModelViewSet):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-_processes = {}
+_ingest_processes = {}
 
 def iter_ingest(ingest):
     '''Ingest into the common schema tables from the DataFiles.'''
@@ -548,7 +548,7 @@ def iter_ingest(ingest):
 
 
 def _update_ingest_progress(ingest_id, file_id, pos, size, processed, total):
-    _processes[ingest_id] = {
+    _ingest_processes[ingest_id] = {
         'id': ingest_id,
         'status': 'processing',
         'percent': processed * 100.0 / total if total else 0.0,
@@ -601,7 +601,7 @@ def perform_ingestion(ingest, batch_size=999, report_interval=1000):
     finally:
         ingest.end = datetime.datetime.utcnow().replace(tzinfo=utc)
         ingest.save()
-        _processes.pop(ingest.id, None)
+        _ingest_processes.pop(ingest.id, None)
 
 
 class DataSetViewSet(viewsets.ModelViewSet):
@@ -613,23 +613,24 @@ class DataSetViewSet(viewsets.ModelViewSet):
     @link(permission_classes = (permissions.IsAuthenticated,))
     def status(self, request, *args, **kwargs):
         ingest = self.get_object()
-        process = _processes.get(ingest.id)
-        if process:
-            return Response(process)
-        return Response({
-            'id': ingest.id,
-            'status': 'complete' if ingest.end else 'incomplete',
-            'percent': 100.0,
-            'current_file_percent': 0.0,
-            'current_file': None
-        })
+        try:
+            process = _ingest_processes[ingest.id]
+        except KeyError:
+            process = {
+                'id': ingest.id,
+                'status': 'complete' if ingest.end else 'incomplete',
+                'percent': 100.0,
+                'current_file_percent': 0.0,
+                'current_file': None
+            }
+        return Response(process)
 
     @link()
     def errors(self, request, *args, **kwargs):
         '''Retrieves all errors that occured during an ingestion.'''
         ingest = self.get_object()
         errors = ingest.logs.all()
-        if not ingest.end and ingest.id not in _processes:
+        if not ingest.end and ingest.id not in _ingest_processes:
             error = models.SensorIngestLog(dataset=ingest, level=models.CRITICAL,
                message='Processing ended prematurely. Not all files and/or '
                        'records were read. Please delete this dataset and '
@@ -888,48 +889,39 @@ class ApplicationViewSet(viewsets.ViewSet):
         return Response(app_list)
 
 
-def _perform_analysis(analysis):
-    '''Create thread for individual runs of an applicaton.
-    '''
+_analysis_processes = set()
 
+def _perform_analysis(analysis):
+    '''Create thread for individual runs of an applicaton.'''
     try:
         analysis.started = datetime.datetime.utcnow().replace(tzinfo=utc)
-        analysis.status = "running"
         analysis.save()
+        try:
+            db_input = DatabaseInput(analysis.dataset.map.id,
+                    analysis.configuration["inputs"], analysis.dataset.id)
+            klass = get_algorithm_class(analysis.application)
+            output_format = klass.output_format(db_input)
+            kwargs = analysis.configuration['parameters']
+            #if analysis.debug:
+            db_output = DatabaseOutputZip(analysis, output_format, analysis.configuration)
+            #else:
+            #    db_output = DatabaseOutput(analysis, output_format)
 
-        dataset_id = analysis.dataset.id
-        datamap_id = analysis.dataset.map.id
-        topic_map = analysis.configuration["inputs"]
-        db_input = DatabaseInput(datamap_id, topic_map, dataset_id)
-
-        klass = get_algorithm_class(analysis.application)
-        output_format = klass.output_format(db_input)
-
-        kwargs = analysis.configuration['parameters']
-        #if analysis.debug:
-        db_output = DatabaseOutputZip(analysis, output_format, analysis.configuration)
-        #else:
-        #    db_output = DatabaseOutput(analysis, output_format)
-
-
-
-        app = klass(db_input, db_output, **kwargs)
-        app.run_application()
-
-    except Exception as e:
-        analysis.status = "error"
-        db_output.appenFileToZip("stackTrace.txt", traceback.format_exc())
+            try:
+                app = klass(db_input, db_output, **kwargs)
+                app.run_application()
+            except Exception:
+                db_output.appenFileToZip("stackTrace.txt", traceback.format_exc())
+                analysis.reports = [serializers.ReportSerializer(report).data
+                                    for report in klass.reports(output_format)]
+        finally:
+            analysis.ended = datetime.datetime.utcnow().replace(tzinfo=utc)
+            analysis.save()
+    except Exception:
         # TODO: log errors
         print(traceback.format_exc())
-
     finally:
-        if analysis.status != "error":
-            analysis.reports = [serializers.ReportSerializer(report).data for
-                                report in klass.reports(output_format)]
-            analysis.status = "complete"
-            analysis.progress_percent = 100
-        analysis.ended = datetime.datetime.utcnow().replace(tzinfo=utc)
-        analysis.save()
+        _analysis_processes.discard(analysis.id)
 
 
 def _get_output_data(request, analysis):
@@ -981,6 +973,11 @@ class AnalysisViewSet(viewsets.ModelViewSet):
             return serializers.AnalysisUpdateSerializer
         return serializers.AnalysisSerializer
 
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context['is_alive'] = lambda obj_id: obj_id in _analysis_processes
+        return context
+
     def pre_save(self, obj):
         '''Check dataset ownership and application existence.'''
         if self.request.method == 'POST':
@@ -992,13 +989,12 @@ class AnalysisViewSet(viewsets.ModelViewSet):
         if not get_algorithm_class(obj.application):
             raise rest_exceptions.ParseError(
                 "Application '{}' not found.".format(obj.application))
-
-
         # TODO: validate dataset and application compatibility
 
     def post_save(self, obj, created):
         '''Start application run after Analysis object has been saved.'''
         if created:
+            _analysis_processes.add(obj.id)
             threading.Thread(
                     target=_perform_analysis, args=(obj,), daemon=True).start()
 

openeis/projects/models.py
@@ -549,13 +549,9 @@ class Analysis(models.Model):
     '''
     configuration = JSONField()
     debug = models.BooleanField(default=False)
-    # Ran successfully or not
-    status = models.CharField(max_length=50, default='queued')
-    # Initially queued
     added = models.DateTimeField(auto_now_add=True)
     started = models.DateTimeField(null=True, default=None)
     ended = models.DateTimeField(null=True, default=None)
-    progress_percent = models.FloatField(default=0)
     reports = JSONField()
 
 

openeis/projects/serializers.py
@@ -362,8 +362,20 @@ class DataSetPreviewSerializer(serializers.Serializer):
 class AnalysisSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Analysis
-        read_only_fields = ('added', 'started', 'ended', 'progress_percent',
-                            'reports', 'status', 'project')
+        read_only_fields = ('added', 'started', 'ended', 'reports', 'project')
+
+    def to_native(self, obj):
+        result = super().to_native(obj)
+        is_alive = self.context.get('is_alive', lambda x: False)
+        if obj and is_alive(obj.id):
+            result['status'] = 'running'
+        elif not obj.started:
+            result['status'] = 'queued'
+        elif not obj.ended:
+            result['status'] = 'incomplete'
+        else:
+            result['status'] = 'complete'
+        return result
 
 class AnalysisUpdateSerializer(AnalysisSerializer):
     class Meta:

commit 619983e2264b7597b25ccbb16ccab51704c621a4

Fix setting of analysis report that was broken by commit 245613f

openeis/projects/views.py
@@ -910,10 +910,10 @@ def _perform_analysis(analysis):
             try:
                 app = klass(db_input, db_output, **kwargs)
                 app.run_application()
-            except Exception:
-                db_output.appenFileToZip("stackTrace.txt", traceback.format_exc())
                 analysis.reports = [serializers.ReportSerializer(report).data
                                     for report in klass.reports(output_format)]
+            except Exception:
+                db_output.appenFileToZip("stackTrace.txt", traceback.format_exc())
         finally:
             analysis.ended = datetime.datetime.utcnow().replace(tzinfo=utc)
             analysis.save()