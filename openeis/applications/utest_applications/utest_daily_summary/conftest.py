0a66729e73dccebeb642458864a88e6628065d65
Craig Allwardt <craig.allwardt@pnnl.gov>
2014-11-09 01:15:42 -0800

    Added other dataset creations for the pytest fixtures.

--- openeis/applications/utest_applications/utest_daily_summary/conftest.py
+++ openeis/applications/utest_applications/utest_daily_summary/conftest.py
@@ -1,9 +1,11 @@
 import os
 import pytest
 
+from openeis.projects import models
 from openeis.applications.utest_applications.conftest import (project,
                                                               create_data_file,
-                                                              create_dataset)  
+                                                              create_dataset,
+                                                              active_user)  
 
 abs_file_dir = os.path.join(os.path.dirname(__file__))
 
@@ -12,3 +14,46 @@ def daily_summary_datafile(project):
     return create_data_file(os.path.join(abs_file_dir, 'daily_summary_data.csv'), 
                             project=project,
                             comments='Daily Summary test data.')
+
+@pytest.fixture
+def daily_summary_sensor_dataset(project, daily_summary_datamap, daily_summary_datafile):
+    return create_dataset(name='Daily Summary Datamap', project=project, 
+                          datamap=daily_summary_datamap, files={'0': daily_summary_datafile})
+    
+@pytest.fixture
+def daily_summary_datamap(project):
+    return models.DataMap.objects.create(project=project, name="Daily Summary Map",
+        map={
+            
+            "sensors": {
+                "lbnl/bldg90/WholeBuildingElectricity": {
+                    "column": "samenumber",
+                    "unit":"kilowatt",
+                    "type": "WholeBuildingElectricity",
+                    "file": "0"
+                },
+                "lbnl":{
+                    "level":"site"
+                }
+            },
+            "files": {
+                "0": {
+                    "signature": {
+                        "headers": [
+                            "datetime", 
+                            "onetofive", 
+                            "withmissing", 
+                            "samenumber", 
+                            "floats", 
+                            "missingandfloats"
+                        ]
+                    },
+                    "timestamp": {
+                        "columns": [
+                            0
+                        ]
+                    }
+                }
+            },
+            "version": 1
+        })
