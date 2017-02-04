fa1a9bab5b939bfd2dbfd0b06f600a9cf15c2de4
Craig Allwardt <craig.allwardt@pnnl.gov>
2017-02-03 19:12:44 -0800

    Added python 3.4 support.

--- bootstrap_openeis.bat
+++ bootstrap_openeis.bat
@@ -3,22 +3,32 @@
 
 C:\Python34\python.exe bootstrap.py
 
-:: Install dependencies for running extra things.
-:: env\Scripts\pip.exe install pyflakes
-:: env\Scripts\pip.exe install pylint
-env\Scripts\pip.exe install pytest-django
+SET ENVPYTHON=env\Scripts\python.exe
+SET ENVPIP=env\Scripts\pip.exe
+SET OPENEIS=env\Scripts\openeis.exe
+
+:: Upgrade pip to the lastes version
+%ENVPYTHON% -m pip install --upgrade pip
+
+:: These are extra things that we want to be installed and either are
+:: available from PyPi or we have in our windows support folder.
+
+%ENVPIP% install workalendar
+%ENVPIP% install pytest-django
+%ENVPIP% install dist/windows-support/pre-built-wheels/numpy-1.12.0+mkl-cp34-cp34m-win32.whl
+%ENVPIP% install dist/windows-support/pre-built-wheels/scipy-0.18.1-cp34-cp34m-win32.whl
+:: %ENVPIP% install pandas
+
+%ENVPIP% install pytest-django
 
 :: Required export so that tests can be run
 set DJANGO_SETTINGS_MODULE=openeis.server.settings
 
 :: Setup the database
-env\Scripts\openeis.exe syncdb --noinput --no-initial-data
+%OPENEIS% syncdb --noinput --no-initial-data
 
 :: Load the exported data
-env\Scripts\openeis.exe loaddata projects_test_auth.json
-
-:: Install numpy for some of the applications to work properly.
-xcopy ..\openeis-setup-support\numpy1.8.2 env\Lib\site-packages /F/E/Y
+:: %OPENEIS% loaddata projects_test_auth.json
 
 ::env\Scripts\py.test openeis\projects --junitxml=xunit-output.xml --ds=openeis.server.settings
 
